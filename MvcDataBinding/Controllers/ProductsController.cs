c0aba5794549011f313ec6fc6a957030c50ff8da
SujithVuppala <ssujith306@gmail.com>
2019-06-03 15:09:36 +0530

    asp.net mvc databinding

---
+++ MvcDataBinding/Controllers/ProductsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDataBinding.Models;
+using System.Net;
+
+
+namespace MvcDataBinding.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductsContext db = new ProductsContext();
+        public ViewResult Index()
+        {
+            return View(db.ProductTable.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Product Id is required");
+
+            }
+            Product product = db.ProductTable.Find(id);
+            if(product==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound, "Product Not Found");
+            }
+            return View(product);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection frmCollection)
+
+        {
+            Product product = new Product();
+            product.Name = frmCollection["Name"];
+            product.Price = Convert.ToDecimal(frmCollection["Price"]);
+
+            db.ProductTable.Add(product);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Product Id is required");
+
+            }
+            Product product = db.ProductTable.Find(id);
+            if (product == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound, "Product Not Found");
+            }
+            return View(product);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(int id)
+        {
+            Product product = db.ProductTable.Find(id);
+            UpdateModel(product);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Product Id is required");
+
+            }
+            Product product = db.ProductTable.Find(id);
+            if (product == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound, "Product Not Found");
+            }
+            return View(product);
+        }
+        [HttpPost]
+        
+         public ActionResult Delete(int id)
+        {
+
+            Product product = db.ProductTable.Find(id);
+            db.ProductTable.Remove(product);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        
+    }
+
+
+
+    }
+}
---
+++ MvcDataBinding/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcDataBinding.Models
+{
+    [Table("tblProducts")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public Nullable<decimal> Price { get; set; }
+    }
+}
